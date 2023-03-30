ba6dc80668f0539b26f5f20b8a69af43663887bc
jjtafur <jjtafur22@gmail.com>
2023-03-30 18:45:31 -0500

    Commit2. Case

--- Juego/Juego/Program.cs
+++ Juego/Juego/Program.cs
@@ -2,11 +2,33 @@
 
 namespace Juego
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random= new Random();
+
+            int players, nummin = 0, nummax = 0;
+
+            Console.WriteLine("Ingrese el número de jugadores: Minimo 2 - Máximo 4");
+            players= Convert.ToInt32(Console.ReadLine());
+
+            switch (players)
+            {
+
+                case 2: nummax = 50;
+                    break;
+
+                case 3:
+                    nummax = 100;
+                    break;
+                case 4:
+                    nummax = 200;
+                    break;
+
+            }
+
+            int secretnum = random.Next(nummin, nummax);
         }
     }
 }
