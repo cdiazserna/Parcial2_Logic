7672ae807dae5dab44fe0434753f1df322c2c510
ricardo taborda palacio <richtp@hotmail.com>
2023-03-30 18:55:27 -0500

    avanzando en el programa

--- Juego/Juego/Program.cs
+++ Juego/Juego/Program.cs
@@ -6,7 +6,20 @@ namespace Juego
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            Console.WriteLine("\n   **********************************");
+            Console.WriteLine("   *Bienvenidos Al Juego Adivina el numero*");
+            Console.WriteLine("   ****************************************\n");
+            Console.WriteLine("    Por Favor Ingrsa El numero de Paticipantes\n");
+            Console.WriteLine("      1-  2 Jugadores.");
+            Console.WriteLine("      2-  3 Jugadores.");
+            Console.WriteLine("      3-  4 Jugadores.");
+
+
+
+
+
         }
     }
 }
