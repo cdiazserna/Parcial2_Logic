60aa46488ec3608991af85b1b5acd47df13a3134
jjtafur <jjtafur22@gmail.com>
2023-03-30 19:58:03 -0500

    Commint5. subprogramas

--- Juego/Juego/Program.cs
+++ Juego/Juego/Program.cs
@@ -6,70 +6,67 @@ namespace Juego
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-
-            int players, nummin = 0, nummax = 0;
-            int secretnum = random.Next(nummin, nummax);
-            int turnplayer = 1;
-
-            AskData(out players, nummin, nummax);
-            Chancenumber(turnplayer, nummin, nummax, secretnum, players);
-
-           
-
-            Console.Write("\nDigite 'y' para intentarlo nuevamente o 'n' para finalizar: ");
-            string newplay = Console.ReadLine();
-        }
-
-        public static void AskData(out int players, int nummin, int nummax)
-        {
-            do { 
-            Console.WriteLine("Ingrese el número de jugadores: Minimo 2 - Máximo 4");
-            players = Convert.ToInt32(Console.ReadLine());
-               } while (players< 2 || players> 4);
-        
-
+            int players = PedirDatos("Ingrese el número de jugadores (entre 2 y 4): ", 2, 4);
+            int nummin = 0, numax = 0;
             switch (players)
             {
-
                 case 2:
-                    nummax = 50;
+                    numax = 50;
                     break;
-
                 case 3:
-                    nummax = 100;
+                    numax = 100;
                     break;
                 case 4:
-                    nummax = 200;
+                    numax = 200;
                     break;
-
+            }
+            int secretNum = CalcularDatos(nummin, numax);
+            MostrarResultado(secretNum, players, nummin, numax);
+            if (PedirDatos("¿Desea jugar de nuevo? (s/n): ", "s", "n") == "s")
+            {
+                Main();
             }
         }
 
-        public static int Chancenumber (int turnplayer, int nummin, int nummax, int secretnum, int players)
-
+        static int PedirDatos(string mensaje, int min, int max)
         {
+            int dato;
+            do
+            {
+                Console.Write(mensaje);
+                dato = Convert.ToInt32(Console.ReadLine());
+            } while (dato < min || dato > max);
+            return dato;
+        }
+
+        static int CalcularDatos(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max + 1);
+        }
+
+        static void MostrarResultado(int secretNum, int players, int nummin, int nummax)
+        {
+            int turnPlayer = 1;
             while (true)
             {
-                Console.WriteLine($"Turno jugador {turnplayer}, ingrese un número entre {nummin} y {nummax}");
-                int chance = Convert.ToInt32(Console.ReadLine());
-
-                if (chance < secretnum)
+                Console.Write($"Jugador {turnPlayer}, ingrese un número entre {nummin} y {nummax}: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if (guess < secretNum)
                 {
-                    Console.Write("MAYOR");
-                    turnplayer = (turnplayer % players) + 1;
+                    Console.WriteLine("MAYOR");
+                    turnPlayer = (turnPlayer % players) + 1;
                 }
-                else if (chance > secretnum)
+                else if (guess > secretNum)
                 {
-                    Console.Write("MENOR");
-                    turnplayer = (turnplayer % players) + 1;
+                    Console.WriteLine("MENOR");
+                    turnPlayer = (turnPlayer % players) + 1;
                 }
                 else
                 {
-                    Console.Write("¡HAS GANADO!");
+                    Console.WriteLine("¡HAS GANADO!");
                     break;
                 }
-                return turnplayer;
             }
         }
     }
