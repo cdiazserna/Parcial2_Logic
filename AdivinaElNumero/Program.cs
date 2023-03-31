d4f24cb7860397b073f360cf4e0b6dd42987d6eb
JAIRO LEANDRO MORA GUERRA <jairomora214411@correo.itm.edu.co>
2023-03-30 19:43:06 -0500

    validacion jugador 1

---
+++ AdivinaElNumero/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdivinaElNumero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
