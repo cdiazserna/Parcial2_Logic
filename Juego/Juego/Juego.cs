1e02d1424a1a856a6a465046f9a13bd9ee4d081c
LEYDI YULIETH PUERTA CAMPO <leydipuerta249947@correo.itm.edu.co>
2023-03-30 19:38:41 -0500

    New Juego

--- Juego/Juego/Juego.cs
+++ Juego/Juego/Juego.cs
@@ -3,36 +3,61 @@ using System.Security.Cryptography;
 
 namespace Juego
 {
-    internal class Juego
+    public class Juego
     {
         static void Main(string[] args)
         {
-            int NumParticipantes, NumAleatorio, NumJugador;
+            int NumAleatorio, NumJugador;
             decimal randomNumber, min, max;
+            int i;
 
             //Clase Random
             Random random = new Random();
             randomNumber = random.Next(min, max);
 
 
-            PedirDatos(out NumParticipantes, out NumAleatorio);
-            ProcJuego();
+
+            PedirDatos(out NumJugador, out NumAleatorio);
+            ProcJuego(out NumJugador, out NumAleatorio);
             MostrarResultado();
 
+
         }
 
-        static void PedirDatos(out int NumParticipantes, out int NumAleatorio)
+        static public void PedirDatos(out int Numjugador, out int NumAleatorio)
         {
-            Console.Write("Por favor ingrese el número de personas a jugar: ");
-            NumParticipantes = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Por favor ingrese el número de jugadores entre 2 y 4: ");
+            Numjugador = Convert.ToInt32(Console.ReadLine());
 
-            
+            Console.Write("Ingrese un número: ");
+            NumAleatorio = Convert.ToInt32(Console.ReadLine());
         }
 
-        static void ProcJuego()
+        static public void ProcJuego(out int Numjugador, out int NumAleatorio, out int i)
+
         {
+            while (Numjugador < 2 || Numjugador > 4)
+            {
+                Console.Write("Por favor ingrese el número de jugadores entre 2 y 4: ");
+                Numjugador = Convert.ToInt32(Console.ReadLine());
 
+                if (Numjugador == 2)
+                {
+                    i = NumAleatorio(0, 50);
+                }
+                else if (Numjugador == 3)
+
+                {
+                    i = numAleatorio(0, 100);
+                }
+                else if (Numjugador == 4)
+                {
+                    i = NumAleatorio(0, 200);
+                    }
+            }
         }
+    }
+}
 
         static void MostrarResultado()
         {
