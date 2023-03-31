488dcb0a2014861f43377c4203d50ec42b4d1720
LEYDI YULIETH PUERTA CAMPO <leydipuerta249947@correo.itm.edu.co>
2023-03-30 19:12:54 -0500

    Juego

--- Juego/Juego/Juego.cs
+++ Juego/Juego/Juego.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace Juego
 {
@@ -7,16 +8,25 @@ namespace Juego
         static void Main(string[] args)
         {
             int NumParticipantes, NumAleatorio, NumJugador;
+            decimal randomNumber, min, max;
 
-            PedirDatos();
+            //Clase Random
+            Random random = new Random();
+            randomNumber = random.Next(min, max);
+
+
+            PedirDatos(out NumParticipantes, out NumAleatorio);
             ProcJuego();
             MostrarResultado();
 
         }
 
-        static void PedirDatos()
+        static void PedirDatos(out int NumParticipantes, out int NumAleatorio)
         {
+            Console.Write("Por favor ingrese el número de personas a jugar: ");
+            NumParticipantes = Convert.ToInt32(Console.ReadLine());
 
+            
         }
 
         static void ProcJuego()
